634977dd838b31776ad3dae390a2f636b7b8d024
Richard Su <rwsu@yahoo.com>
2013-07-09 03:54:36 +0000

    web.js reads index.html

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(fs.readFile("index.html").toString());
 });
 
 var port = process.env.PORT || 5000;
